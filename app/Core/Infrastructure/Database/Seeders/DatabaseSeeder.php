e3c4a2bc80b449a9460b782fd320ff745ada44c6
Griftor0 <abievsultann@mail.ru>
2026-01-04 21:18:55 +0700

    rewrite Laravel to DDD with core

---
+++ app/Core/Infrastructure/Database/Seeders/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Core\Infrastructure\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    public function run(): void
+    {
+        $this->call([
+
+        ]);
+    }
+}
---
+++ app/Core/Infrastructure/Providers/AppServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Core\Infrastructure\Providers;
+
+use Illuminate\Support\Carbon;
+use Illuminate\Support\ServiceProvider;
+
+final class AppServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        $locale = config('app.locale');
+
+        setlocale(LC_ALL, $locale . '.utf8');
+        Carbon::setLocale($locale);
+
+        $this->loadMigrationsFrom(
+            base_path('app/Core/Infrastructure/Database/Migrations')
+        );
+    }
+}
